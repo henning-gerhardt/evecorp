f5944e0c37e78b0c324d41e0815b154a5cf6afa8
Henning Gerhardt <henning.gerhardt@web.de>
2014-03-27 09:51:12 +0100

    Fix creating of PhealNG namespace.
    Works now with Typo3 6.1.x and 6.2.0!

--- ext_autoload.php
+++ ext_autoload.php
@@ -1,26 +1,26 @@
 <?php
 $extPath =  \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('evecorp');
 $srcPath = $extPath . 'Pheal/';
-$prefix  = 'pheal_';
+$prefix  = 'Pheal\\';
 $default = array();
 
 $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($srcPath));
 $regex = new RegexIterator($iterator, '/^.+\.php$/i', RecursiveRegexIterator::GET_MATCH);
 
 foreach ($regex as $key => $value) {
-    $file = realpath($key);
-    $basename = basename($file);
-    $pureBasename = basename($file, '.php');
-    $parentDirname = basename(dirname($file));
-    if ($parentDirname === 'Pheal') {
-        $newkey = $prefix . strtolower($pureBasename);
-        $newvalue = $basename;
-    } else {
-		$newkey = $prefix . strtolower($parentDirname) .'_' . strtolower($pureBasename);
-        $newvalue = $parentDirname . DIRECTORY_SEPARATOR . $basename;
+	$file = realpath($key);
+	$basename = basename($file);
+	$pureBasename = basename($file, '.php');
+	$parentDirname = basename(dirname($file));
+	if ($parentDirname === 'Pheal') {
+		$newkey = $prefix . $pureBasename;
+		$newvalue = $basename;
+	} else {
+		$newkey = $prefix . $parentDirname .'\\' . $pureBasename;
+		$newvalue = $parentDirname . DIRECTORY_SEPARATOR . $basename;
 	}
-    
-    $default[$newkey] = realpath($srcPath . DIRECTORY_SEPARATOR . $newvalue);
+
+	$default[$newkey] = realpath($srcPath . DIRECTORY_SEPARATOR . $newvalue);
 }
 
 return $default;
