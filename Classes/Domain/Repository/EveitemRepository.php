35333899e42f886ee43c1519ea72937190d4a7f5
Henning Gerhardt <henning.gerhardt@web.de>
2014-03-29 17:07:31 +0100

    Fix #7: Add support for different system id
    Made update task avaible of different system ids

--- Classes/Domain/Repository/EveitemRepository.php
+++ Classes/Domain/Repository/EveitemRepository.php
@@ -4,8 +4,8 @@ namespace gerh\Evecorp\Domain\Repository;
 /***************************************************************
  *  Copyright notice
  *
- *  (c) 2013 - 2014 Henning Gerhardt 
- *  
+ *  (c) 2013 - 2014 Henning Gerhardt
+ *
  *  All rights reserved
  *
  *  This script is part of the TYPO3 project. The TYPO3 project is
@@ -33,10 +33,10 @@ namespace gerh\Evecorp\Domain\Repository;
  *
  */
 class EveitemRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
-	
+
 	/**
 	 * Find all updateable eve items
-	 * 
+	 *
 	 * @param int $timeToCache
 	 * @return \TYPO3\CMS\Extbase\Persistence\QueryResultInterface|array
 	 */
@@ -57,4 +57,85 @@ class EveitemRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
 		return $query->execute();
 	}
 
+	/**
+	 * Get array of unique used system ids
+	 * 
+	 * @return array
+	 */
+	public function getListOfUniqueSystemId() {
+
+		/** @var $query \TYPO3\CMS\Extbase\Persistence\QueryInterface */
+		$query = $this->createQuery();
+		$query->getQuerySettings()->setReturnRawQueryResult(true);
+
+		/** todo replace if possible with matching query */
+		$statement = 'SELECT DISTINCT `system_id` FROM `tx_evecorp_domain_model_eveitem` ';
+		$statement .= ' WHERE (`deleted` = 0) AND (`hidden` = 0) ';
+		$rowData = $query->statement($statement)->execute();
+
+		// own data mapping
+		$result = array();
+		foreach($rowData as $rows) {
+			foreach($rows as $columnValue) {
+				$result[] = $columnValue;
+			}
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Search for all out of date EVE items for a given system
+	 * 
+	 * @param \integer $systemId
+	 * @return \TYPO3\CMS\Extbase\Persistence\QueryResultInterface|array
+	 */
+	public function findAllUpdateableItemsForSystem($systemId) {
+
+		if ($systemId == null || $systemId == 0) {
+			return new \TYPO3\CMS\Extbase\Persistence\Generic\QueryResult();
+		}
+
+		/** @var $query \TYPO3\CMS\Extbase\Persistence\QueryInterface */
+		$query = $this->createQuery();
+
+		/** @todo replace if possible with matching query */
+		$statement = 'SELECT * FROM `tx_evecorp_domain_model_eveitem` ';
+		$statement .= ' WHERE `cache_time` < (UNIX_TIMESTAMP() - (`time_to_cache` * 60)) ';
+		$statement .= ' AND (`system_id` = ?) ';
+		$statement .= ' AND (`deleted` = 0) AND (`hidden` = 0) ';
+
+		$query->statement($statement, array((int)$systemId));
+
+		/** @var $result \TYPO3\CMS\Extbase\Persistence\Generic\QueryResult */
+		$result = $query->execute();
+
+		return $result;
+	}
+
+	/**
+	 * Search for a specific EVE item and system
+	 * 
+	 * @param \integer $eveId
+	 * @param \integer $systemId
+	 * @param \boolean $respectStoragePage
+	 * @return \TYPO3\CMS\Extbase\Persistence\QueryResultInterface|array
+	 */
+	public function findByEveIdAndSystemId($eveId, $systemId, $respectStoragePage = false) {
+		/** @var $query \TYPO3\CMS\Extbase\Persistence\QueryInterface */
+		$query = $this->createQuery();
+
+		$query->matching($query->logicalAnd(
+			$query->equals('EveId', $eveId),
+			$query->equals('SystemId', $systemId)
+		));
+
+		// using or ignoring of storage page must be set after matching query
+		$query->getQuerySettings()->setRespectStoragePage($respectStoragePage);
+
+		/** @var $result \TYPO3\CMS\Extbase\Persistence\Generic\QueryResult */
+		$result = $query->execute();
+		return $result;
+	}
+	
 }
--- Classes/Task/UpdateEveItemListTask.php
+++ Classes/Task/UpdateEveItemListTask.php
@@ -35,53 +35,106 @@ namespace gerh\Evecorp\Task;
 class UpdateEveItemListTask extends \TYPO3\CMS\Scheduler\Task\AbstractTask {
 
 	/**
+	 * @var $eveItemRepository \gerh\Evecorp\Domain\Repository\EveitemRepository
+	 */
+	protected $eveItemRepository;
+
+	/**
+	 * @var $eveCentralFetcher \gerh\Evecorp\Domain\Model\EveCentralFetcher
+	 */
+	protected $eveCentralFetcher;
+
+	/**
 	 * Update outdated stored EVE item
 	 *
 	 * @return void
 	 */
 	protected function updateEveItemList() {
-		$extconf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['evecorp']);
-
-		$eveCentralUri = $extconf['evecentralUri'];
-		/** @todo replace with configurable value */
-		$systemId = 30000142;
-		/** @todo replace with configurable value */
-		$timeToCache = 5;
 
 		/** @var $objectManager \TYPO3\CMS\Extbase\Object\ObjectManager */
 		$objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager');
+		$this->eveItemRepository = $objectManager->get('gerh\\Evecorp\\Domain\\Repository\\EveitemRepository');
+		$this->eveCentralFetcher = $objectManager->get('gerh\\Evecorp\\Domain\\Model\\EveCentralFetcher');
 
-		/** @var $eveItemRepository \gerh\Evecorp\Domain\Repository\EveitemRepository */
-		$eveItemRepository = $objectManager->get('gerh\\Evecorp\\Domain\\Repository\\EveitemRepository');
+		$extconf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['evecorp']);
+		$this->eveCentralFetcher->setBaseUri($extconf['evecentralUri']);
 
-		// get out dated items
-		$fetchList = array();
-		$dbEntryList = $eveItemRepository->findAllUpdateableItems($timeToCache);
-		foreach($dbEntryList as $entry) {
-			$fetchList[$entry->getEveId()] = $entry->getEveName();
-		}
+		foreach($this->eveItemRepository->getListOfUniqueSystemId() as $systemId) {
 
-		// fetch current values for this items
-		/** @var $fetcher \gerh\Evecorp\Domain\Model\EveCentralFetcher */
-		$fetcher = $objectManager->get('gerh\\Evecorp\\Domain\\Model\\EveCentralFetcher');
-		$fetcher->setBaseUri($eveCentralUri);
-		$fetcher->setSystemId($systemId);
-		$fetcher->setTypeIds($fetchList);
-		$newValuesForDb = $fetcher->query();
+			// get out dated items
+			$listOfOutdatedItems = $this->getListOfOutdatedItemsForSystem($systemId);
 
-		// update database
-		foreach($newValuesForDb as $eveId => $values) {
-			foreach($eveItemRepository->findByEveId($eveId) as $dbEntry) {
-				$dbEntry->setBuyPrice($values['buy']);
-				$dbEntry->setSellPrice($values['sell']);
-				$dbEntry->setCacheTime(time());
-				$eveItemRepository->update($dbEntry);
-			}
+			// fetch current values for this items
+			$newValuesForDb = $this->fetchUpdateableItems($listOfOutdatedItems, $systemId);
+
+			// update database
+			$this->updateEveItemsForSystem($newValuesForDb, $systemId);
 		}
 
 		/** @var $persistenceManager \TYPO3\CMS\Extbase\Persistence\Generic\PersistenceManager */
 		$persistenceManager = $objectManager->get('TYPO3\\CMS\\Extbase\\Persistence\\Generic\\PersistenceManager');
 		$persistenceManager->persistAll();
+	}
+
+	/**
+	 * Get list of out dated EVE items for a specific system
+	 *
+	 * @param \integer $systemId
+	 * @return array
+	 */
+	protected function getListOfOutdatedItemsForSystem($systemId) {
+		$fetchList = array();
+		
+		if (($systemId == null) || ($systemId == 0)) {
+			return $fetchList;
+		}
+
+		foreach($this->eveItemRepository->findAllUpdateableItemsForSystem($systemId) as $entry) {
+			$fetchList[$entry->getEveId()] = $entry->getEveName();
+		}
+		return $fetchList;
+	}
+
+	/**
+	 * Fetch current EVE item values for a specific system
+	 *
+	 * @param array $fetchList
+	 * @param \integer $systemId
+	 * @return array
+	 */
+	protected function fetchUpdateableItems($fetchList, $systemId) {
+		
+		if ((count($fetchList) == 0) || ($systemId == null) || ($systemId == 0)) {
+			return array();
+		}
+		
+		$this->eveCentralFetcher->setSystemId($systemId);
+		$this->eveCentralFetcher->setTypeIds($fetchList);
+		$result = $this->eveCentralFetcher->query();
+		
+		return $result;
+	}
+
+	/**
+	 * Update changed EVE items with new values
+	 * 
+	 * @param array $eveItemUpdateList
+	 * @param \integer $systemId
+	 */
+	protected function updateEveItemsForSystem($eveItemUpdateList, $systemId) {
+		
+		if ((count($eveItemUpdateList) == 0) || ($systemId == null) || $systemId == 0) {
+			return;
+		}
+
+		foreach($eveItemUpdateList as $eveId => $values) {
+			foreach($this->eveItemRepository->findByEveIdAndSystemId($eveId, $systemId) as $dbEntry) {
+				$dbEntry->setBuyPrice($values['buy']);
+				$dbEntry->setSellPrice($values['sell']);
+				$dbEntry->setCacheTime(time());
+				$this->eveItemRepository->update($dbEntry);
+			}
+		}
 	}
 
 	/**
