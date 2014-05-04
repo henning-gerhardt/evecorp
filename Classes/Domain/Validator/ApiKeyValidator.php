1aa158f0ded1e446d33045b877f7c828b5d0874f
Henning Gerhardt <henning.gerhardt@web.de>
2014-05-04 13:33:16 +0200

    Rewrote and extending of APIKeyValidator

--- Classes/Domain/Validator/ApiKeyValidator.php
+++ Classes/Domain/Validator/ApiKeyValidator.php
@@ -35,70 +35,132 @@ namespace Gerh\Evecorp\Domain\Validator;
 class ApiKeyValidator extends \TYPO3\CMS\Extbase\Validation\Validator\AbstractValidator {
 
 	/**
-	 * Check given credentials against CCP API server for an account type.
-	 *
-	 * @param type $keyId
-	 * @param type $vCode
-	 * @return boolean
+	 * @var \Gerh\Evecorp\Domain\Repository\ApiKeyRepository
+	 * @inject
 	 */
-	protected function checkApiKeys($keyId, $vCode) {
-		$scope = 'account';
+	protected $apiKeyRepository;
 
-		try {
-			$phealService = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('Gerh\Evecorp\Service\PhealService', $keyId, $vCode, $scope);
-			$pheal = $phealService->getPhealInstance();
-			$response = $pheal->accountScope->APIKeyInfo();
-			if ($response->key->type !== 'Account') {
-				$this->addError('Given key is not an account key', 123456890);
-				return FALSE;
-			} else {
-				return TRUE;
-			}
-		} catch (\Pheal\Exceptions\PhealException $ex) {
-			$this->addError($ex->getMessage(), 123456890);
-			return FALSE;
+	/**
+	 * @var \Gerh\Evecorp\Domain\Repository\CharacterRepository
+	 * @inject
+	 */
+	protected $characterRepository;
+
+	/**
+	 * Is given type equal expected type. Expected type default is 'Account'.
+	 *
+	 * @param \string $actualType   Current API key type
+	 * @param \string $expectedType To check against. Default: 'Account'
+	 * @return \boolean
+	 */
+	protected function isApiType($actualType, $expectedType = 'Account') {
+		return ($actualType === $expectedType);
+	}
+
+	/**
+	 * Check if given character id is already stored in database
+	 *
+	 * @param \integer $characterId
+	 * @return \boolean
+	 */
+	protected function isCharacterIdAlreadyInDatabase($characterId) {
+		$result = $this->characterRepository->countByCharacterId($characterId);
+		if ($result > 0) {
+			return TRUE;
 		}
 
 		return FALSE;
 	}
 
 	/**
+	 * Check for :
+	 *  - Given API key against CCP API server for correct API key type
+	 *  - API key based characters are not already stored in database
+	 *
+	 * @param \integer $keyId
+	 * @param \string $vCode
+	 * @return \boolean
+	 */
+	protected function checkApiKey($keyId, $vCode) {
+		$scope = 'account';
+
+		try {
+			$phealService = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('Gerh\\Evecorp\\Service\\PhealService', $keyId, $vCode, $scope);
+			$pheal = $phealService->getPhealInstance();
+			$response = $pheal->accountScope->APIKeyInfo();
+
+			if (! $this->isApiType($response->key->type, 'Account')) {
+				$this->addError('Given API key is not an Account API key.', 123456890);
+				return FALSE;
+			}
+
+			foreach($response->key->characters as $characterInfo) {
+				if ($this->isCharacterIdAlreadyInDatabase($characterInfo->characterID)) {;
+					$this->addError('Character "' . $characterInfo->characterName . '" is already in database.', 1234567890);
+					return FALSE;
+				}
+			}
+		} catch (\Pheal\Exceptions\PhealException $ex) {
+			$this->addError($ex->getMessage(), 123456890);
+			return FALSE;
+		}
+
+		return TRUE;
+	}
+
+	/**
 	 * Check if given key already be used by somebody else
 	 *
 	 * @param \integer $keyId
-	 * @return boolean
+	 * @return \boolean
 	 */
-	protected function checkForAlreadyExistingKeyIdInDatabase($keyId) {
-		$apiKeyRepository = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('Gerh\Evecorp\Domain\Repository\ApiKeyRepository');
+	protected function isKeyIdAlreadyInDatabase($keyId) {
 
-		$result = $apiKeyRepository->countByKeyId($keyId);
+		$result = $this->apiKeyRepository->countByKeyId($keyId);
 		if ($result > 0) {
-			$this->addError('Key already stored in database', 1234567890);
-			return FALSE;
-		} else {
 			return TRUE;
 		}
+
+		return FALSE;
 	}
 
 	/**
 	 * Made some checks for given model to be valid
 	 *
 	 * @param \Gerh\Evecorp\Domain\Model\ApiKey $value
-	 * @return boolean
+	 * @return \boolean
 	 */
 	protected function isValid($value) {
 
-		if (!$value instanceof \Gerh\Evecorp\Domain\Model\ApiKey) {
+		if (($value instanceof \Gerh\Evecorp\Domain\Model\ApiKey) === FALSE) {
 			$this->addError('Given object has wrong type!', 1234567890);
 			return FALSE;
 		}
 
-		if (empty($value->getKeyId()) || empty($value->getVCode())) {
-			$this->addError('Key ID or Verification Code are empty!', 1234567890);
+		$keyId = $value->getKeyId();
+		$vCode = $value->getVCode();
+
+		if (empty($keyId) === TRUE) {
+			$this->addError('Key ID is empty!', 1234567890);
 			return FALSE;
 		}
 
-		return $this->checkForAlreadyExistingKeyIdInDatabase($value->getKeyId()) && $this->checkApiKeys($value->getKeyId(), $value->getVCode());
+		if (\is_int($keyId) === FALSE) {
+			$this->addError('Key ID is not a integer value!', 1234567890);
+			return FALSE;
+		}
+
+		if (empty($vCode) === TRUE) {
+			$this->addError('Verification code is empty!', 1234567890);
+			return FALSE;
+		}
+
+		if ($this->isKeyIdAlreadyInDatabase($keyId)) {
+			$this->addError('Key already stored in database', 1234567890);
+			return FALSE;
+		}
+
+		return  $this->checkApiKey($keyId, $vCode);
 	}
 
 }
---
+++ Tests/Domain/Validator/ApiKeyValidatorTest.php
@@ -0,0 +1,189 @@
+<?php
+namespace Gerh\Evecorp\Test\Domain\Validator;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2014 Henning Gerhardt
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ *
+ *
+ * @package evecorp
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ *
+ */
+class ApiKeyValidatorTest extends \TYPO3\CMS\Extbase\Tests\Unit\Validation\Validator\AbstractValidatorTestcase {
+
+	/**
+	 * @var \string
+	 */
+	protected $validatorClassName = 'Gerh\\Evecorp\\Domain\\Validator\\ApiKeyValidator';
+
+	/**
+	 * Testsuite setup
+	 */
+	public function setup() {
+		$this->validator = $this->getMock($this->validatorClassName, array('translateErrorMessage'));
+	}
+
+	/**
+	 * Data provider with invalid validation types
+	 * @return array
+	 */
+	public function invalidValidationTypes() {
+		return array(
+			array('string'),
+			array(12345),
+			array(new \stdClass()),
+		);
+	}
+
+	/**
+	 * @test
+	 * @dataProvider invalidValidationTypes
+	 * @param mixed $types
+	 */
+	public function wrongValidationTypeResultsInFailure($types) {
+		$this->assertTrue($this->validator->validate($types)->hasErrors());
+	}
+
+	/**
+	 * Data provider with invalid key
+	 * @return array
+	 */
+	public function invalidKeyIdOrVCode() {
+		return array(
+			array('', ''),
+			array(12, ''),
+			array('12', ''),
+			array(12.5, ''),
+		);
+	}
+
+	/**
+	 * @test
+	 * @dataProvider invalidKeyIdOrVCode
+	 * @param mixed $keyId
+	 * @param mixed $vCode
+	 */
+	public function apiKeyValidatorReturnFalseForInvalidKeyIdOrVCode($keyId, $vCode) {
+		$apiKeyModel = new \Gerh\Evecorp\Domain\Model\ApiKey();
+		$apiKeyModel->setKeyId($keyId);
+		$apiKeyModel->setVCode($vCode);
+
+		$this->assertTrue($this->validator->validate($apiKeyModel)->hasErrors());
+	}
+
+	/**
+	 * Data provder for different count ofkey ids
+	 *
+	 * @return array
+	 */
+	public function countOfKeyIds () {
+		return array(
+			array(0, false),
+			array(1, true)
+		);
+	}
+
+	/**
+	 * @test
+	 * @dataProvider countOfKeyIds
+	 * @param \integer $returnValue
+	 * @param \boolean $expected
+	 */
+	public function checkIsKeyAlreadyInDatabase($returnValue, $expected) {
+		$mockObjectManager = $this->getMock('TYPO3\\CMS\\Extbase\\Object\\ObjectManagerInterface');
+		$mockedRepository = $this->getMock('Gerh\\Evecorp\\Domain\\Repository\\ApiKeyRepository', array('countByKeyId'), array($mockObjectManager));
+		$mockedRepository
+			->expects($this->once())
+			->method('countByKeyId')
+			->will($this->returnValue($returnValue));
+
+		$this->inject($this->validator, 'apiKeyRepository', $mockedRepository);
+
+		$actual = $this->callInaccessibleMethod($this->validator, 'isKeyIdAlreadyInDatabase', 1234567890);
+
+		$this->assertEquals($expected, $actual);
+	}
+
+	/**
+	 * Data provider for different api keys
+	 *
+	 * @return array
+	 */
+	public function apiTypes() {
+		return array(
+			array('Account', 'Account', true),
+			array('Character', 'Account', false),
+			array('Corporation', 'Account', false),
+		);
+	}
+
+	/**
+	 * @test
+	 * @dataProvider apiTypes
+	 * @param \string $actualType
+	 * @param \string $expectedType
+	 * @param \boolean $expectedValue
+	 */
+	public function checkCorrectApiType($actualType, $expectedType, $expectedValue) {
+		$actualValue = $this->callInaccessibleMethod($this->validator, 'isApiType', $actualType, $expectedType);
+
+		$this->assertEquals($expectedValue, $actualValue);
+	}
+
+	/**
+	 * Data provider for different count of character ids
+	 *
+	 * @return array
+	 */
+	public function countOfCharacterIds() {
+		return array(
+			array(0, false),
+			array(1, true),
+		);
+	}
+
+	/**
+	 * @test
+	 * @dataProvider countOfCharacterIds
+	 * @param \integer $returnValue
+	 * @param \boolean $expected
+	 */
+	public function checkIsCharacterAlreadyInDatabase($returnValue, $expected) {
+		$mockObjectManager = $this->getMock('TYPO3\\CMS\\Extbase\\Object\\ObjectManagerInterface');
+		$mockedRepository = $this->getMock('Gerh\\Evecorp\\Domain\\Repository\\CharacterRepository', array('countByCharacterId'), array($mockObjectManager));
+		$mockedRepository
+			->expects($this->once())
+			->method('countByCharacterId')
+			->will($this->returnValue($returnValue));
+
+		$this->inject($this->validator, 'characterRepository', $mockedRepository);
+
+		$actual = $this->callInaccessibleMethod($this->validator, 'isCharacterIdAlreadyInDatabase', 1234567890);
+
+		$this->assertEquals($expected, $actual);
+	}
+
+}
