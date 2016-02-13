c3c8a32a6ba243014f684917b7598478b7b09693
Henning Gerhardt <henning.gerhardt@web.de>
2016-02-13 18:43:18 +0100

    Adjust class auto loading.

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,22 +25,27 @@
  * ************************************************************* */
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Eve Online Corp Tool',
-	'description' => 'Using different EVE services: direct from CCP through PhealNG or third party sites like eve-central.com',
-	'category' => 'plugin',
-	'author' => 'Henning Gerhardt',
-	'author_company' => '',
-	'author_email' => 'henning.gerhardt@web.de',
-	'dependencies' => 'extbase,fluid',
-	'clearCacheOnLoad' => 1,
-	'state' => 'alpha',
-	'version' => '0.5.3',
-	'constraints' => array(
-		'depends' => array(
-			'php' => '5.4.0',
-			'typo3' => '6.2.0-6.2.99',
-			'extbase' => '6.2.0-6.2.99',
-			'fluid' => '6.2.0-6.2.99',
-		),
-	)
+    'title' => 'Eve Online Corp Tool',
+    'description' => 'Using different EVE services: direct from CCP through PhealNG or third party sites like eve-central.com',
+    'category' => 'plugin',
+    'author' => 'Henning Gerhardt',
+    'author_company' => '',
+    'author_email' => 'henning.gerhardt@web.de',
+    'dependencies' => 'extbase,fluid',
+    'clearCacheOnLoad' => 1,
+    'state' => 'alpha',
+    'version' => '0.5.3',
+    'constraints' => array(
+        'depends' => array(
+            'php' => '5.4.0',
+            'typo3' => '6.2.0-6.2.99',
+            'extbase' => '6.2.0-6.2.99',
+            'fluid' => '6.2.0-6.2.99',
+        ),
+    ),
+    'autoload' => array(
+        'psr-4' => array(
+            'Gerh\\Evecorp\\' => 'Classes',
+        )
+    )
 );
