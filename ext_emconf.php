9bee39d12273639a56321a9a19dad8802553165d
Henning Gerhardt <henning.gerhardt@web.de>
2016-02-13 18:50:39 +0100

    Extending used typo3 extensions for typo3 7 lts.

--- ext_emconf.php
+++ ext_emconf.php
@@ -38,9 +38,9 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'php' => '5.4.0',
-            'typo3' => '6.2.0-6.2.99',
-            'extbase' => '6.2.0-6.2.99',
-            'fluid' => '6.2.0-6.2.99',
+            'typo3' => '6.2.0-7.6.99',
+            'extbase' => '6.2.0-7.6.99',
+            'fluid' => '6.2.0-7.6.99',
         ),
     ),
     'autoload' => array(
