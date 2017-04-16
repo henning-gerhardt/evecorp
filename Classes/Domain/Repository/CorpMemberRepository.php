80095d49d0c14d332612b4f6c45b1c58fcdd4ac4
Henning Gerhardt <henning.gerhardt@web.de>
2017-04-16 11:27:45 +0200

    Update copyright notice

--- Classes/Domain/Repository/CorpMemberRepository.php
+++ Classes/Domain/Repository/CorpMemberRepository.php
@@ -1,28 +1,21 @@
 <?php
 
-/* * *************************************************************
- *  Copyright notice
+/*
+ * Copyright notice
  *
- *  (c) 2016 Henning Gerhardt
+ * (c) 2017 Henning Gerhardt
  *
- *  All rights reserved
+ * This file is part of the TYPO3 CMS project.
  *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 3
+ * of the License, or any later version.
  *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
  *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- * ************************************************************* */
+ * The TYPO3 project - inspiring people to share!
+ */
 
 namespace Gerh\Evecorp\Domain\Repository;
 
