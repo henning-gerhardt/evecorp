5e89f9909aa43f0b8fa654e5e99c1cc98f751108
Henning Gerhardt <henning.gerhardt@web.de>
2017-10-14 23:07:40 +0200

    Update language information for hidden column

--- Configuration/TCA/tx_evecorp_domain_model_evemapregion.php
+++ Configuration/TCA/tx_evecorp_domain_model_evemapregion.php
@@ -49,7 +49,7 @@ return [
     'columns' => [
         'hidden' => [
             'exclude' => 1,
-            'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.hidden',
+            'label' => 'LLL:EXT:lang/Resources/Private/Language/locallang_general.xlf:LGL.hidden',
             'config' => [
                 'type' => 'check'
             ]
